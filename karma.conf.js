b5b14699cb2f8a163c0b2261296c20f4df83da7b
rodoabad <rodoabad@gmail.com>
2015-11-05 22:04:13 -0600

    Removed old Karma configs.

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
     ],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_ERROR,
+    logLevel: config.LOG_DEBUG,
     autoWatch: false,
     browsers: [
       'PhantomJS'
@@ -47,9 +47,6 @@ module.exports = function (config) {
           subdir: '.'
         }
       ]
-    },
-    ngHtml2JsPreprocessor: {
-      moduleName: 'karma.templates'
     },
     babelPreprocessor: {
       options: {
